4e90780d644893fa9253582f25edf912e75effe6
borntofrappe <borntofrappe@gmail.com>
2019-05-15 00:41:52 +0200

    complete ui project

--- UI Booking Tickets/README.md
+++ UI Booking Tickets/README.md
@@ -1,8 +1,7 @@
 # UI - Tickets Booking
 
-<!-- ## Live Demo -->
+## [Live Demo](https://codepen.io/borntofrappe/full/dENMNV)
 
 ## Goal
 
 Create the UI for a mobile application allowing to book seats for a hypothetical movie. Inspired by [this particular design](https://dribbble.com/shots/6418198-Tickets-booking).
-
--- UI Booking Tickets/script.js
+++ UI Booking Tickets/script.js
@@ -1,23 +1,23 @@
 // target the grid of seats
 const seats = document.querySelector('.seats');
 
+// possible values to differentiate the seats
 const possibleSeats = [
   'available',
   'reserved',
   'selected',
 ];
+// function to return a random item from the possibleSeats array
 const randomSeat = () => possibleSeats[Math.floor(Math.random() * possibleSeats.length)];
 
-// add 10 rows and 10 columns of buttons
-for (let row = 0; row < 10; row += 1) {
-  for (let column = 0; column < 10; column += 1) {
-    const seat = document.createElement('button');
-    const possibleSeat = randomSeat();
-    seat.innerHTML = `
+// add 86 seats
+for (let i = 0; i < 88; i += 1) {
+  const seat = document.createElement('button');
+  const possibleSeat = randomSeat();
+  seat.innerHTML = `
     <svg class="${possibleSeat}" viewBox="0 0 100 100" width="32" height="32">
       <use href="#${possibleSeat}"></use>
     </svg>
     `;
-    seats.appendChild(seat);
-  }
+  seats.appendChild(seat);
 }
