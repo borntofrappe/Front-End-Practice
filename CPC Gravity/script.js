44d10754c40480ddc73e77d10093806f39dcd94a
borntofrappe <borntofrappe@gmail.com>
2019-05-17 00:10:24 +0200

    document codebase

--- CPC Gravity/script.js
+++ CPC Gravity/script.js
@@ -1,6 +1,7 @@
-const selectionPlanets = document.querySelector('.selection--planets');
-
-const planets = [
+// node in which to add the buttons
+const selectionPlanets = document.querySelector('.selection--bodies');
+// data for the celestial bodies
+const bodies = [
   {
     name: 'mercury',
     gravity: 3.7,
@@ -39,39 +40,48 @@ const planets = [
   },
 ];
 
+// function to update the gravity in the paragraph element
 function updateGravity(gravity) {
   const gameGravity = document.querySelector('.game--gravity strong');
   gameGravity.textContent = gravity;
 }
+// function to update the name in the heading element
 function updateName(name) {
   const selectionLocation = document.querySelector('.selection--location');
   selectionLocation.textContent = name;
 }
-
+// function to add a class of active on the input element
 function selectButton(btn) {
-  const buttons = document.querySelectorAll('.selection--planets button');
+  const buttons = document.querySelectorAll('.selection--bodies button');
   buttons.forEach(button => button.classList.remove('active'));
   btn.classList.add('active');
 }
 
-
-function updateGame() {
-  const { name, gravity } = planets.find(planet => planet.name === this.getAttribute('data-name'));
+// function called when a click event is registered on a button
+function updateSelection() {
+  // retrieve the name, gravity of the matching celestial body
+  const { name, gravity } = bodies.find(body => body.name === this.getAttribute('data-name'));
+  // update the UI with the fabricated functions
   updateGravity(gravity);
   updateName(name);
   selectButton(this);
 }
 
-planets.forEach(({ name }) => {
+// for each celestial body create a button and add it to the selected node
+bodies.forEach(({ name }) => {
   const planetButton = document.createElement('button');
-  planetButton.classList.add('planets--planet');
+  planetButton.classList.add('bodies--body');
+  // add the class of active on the default celestial body
   if (name === 'earth') {
     planetButton.classList.add('active');
   }
   planetButton.setAttribute('data-name', name);
+
+  // add the svg icon through the matching symbol
   planetButton.innerHTML = `<svg width="50" height="50">
     <use href=#${name}></use>
   </svg>`;
-  planetButton.addEventListener('click', updateGame);
+  // fire the updateSelection function on click
+  planetButton.addEventListener('click', updateSelection);
   selectionPlanets.appendChild(planetButton);
 });
