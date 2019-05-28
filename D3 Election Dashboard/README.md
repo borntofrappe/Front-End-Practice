00602671788f3ce18a861046bc6e8338ee8feb30
borntofrappe <borntofrappe@gmail.com>
2019-05-28 12:00:45 +0200

    complete the donut chart

--- D3 Election Dashboard/README.md
+++ D3 Election Dashboard/README.md
@@ -3,3 +3,11 @@
 ## Goal
 
 Inspired by the recent European elections, display the composition of the EU parliament through a series of visualizations.
+
+## Visualizations
+
+The dashboard highlights two visualizations:
+
+- a donut chart (half a donut chart in actuality) displaying the current composition of the European parliament. In addition to the d3-powered visualization, this chart is complemented by a legend, highighting the connected slices through color.
+
+- a row chart (two row charts to be precise) displaying the number of seats accummulated by the parties in the previous and current elections.
--- D3 Election Dashboard/script.js
+++ D3 Election Dashboard/script.js
@@ -70,12 +70,67 @@ const parliament = {
     },
   ],
 };
+// data describing the parties from left to right
+const affiliation = [
+  'Radical left',
+  'Social democrats',
+  'Ecologists and regionalists',
+  'Centrists and liberals',
+  'Conservatives',
+  'Nationalist right and sovereignist',
+  'Extreme right',
+  'Others'
+];
+// data describing the color palette for each party
+const colors = [
+  '#D6001A',
+  '#FF6D90',
+  '#64CC76',
+  '#FF7F00',
+  '#2495C1',
+  '#02279C',
+  '#671706',
+  '#C0C2C9',
+];
+
+/* data massaging:
+  from the parliament array
+  - sort according to the left to right affiliation
+  - add the color matching the party
+
+  finally data ought to look as follows
+
+  - objects for the entering and exiting parliament
+    - array of parties sorted by affiliation
+      - party name
+      - seat count
+      - party color
+*/
+const data = {};
+const elections = Object.entries(parliament);
+elections.forEach(([election, results]) => {
+  Object.assign(data, {
+    [election]: results
+      .sort(({party: partyA}, {party: partyB}) => {
+        const affiliationA = affiliation.findIndex(party => party === partyA);
+        const affiliationB = affiliation.findIndex(party => party === partyB);
+        return affiliationA > affiliationB ? 1 : -1;
+      })
+      .map(({party, seats}) => ({
+        party,
+        seats,
+        color: colors[affiliation.findIndex(aff => aff === party)]
+      })
+    )
+  })
+});
 
 
 // function adding the donut chart
 function showDonutChart() {
-  // the donut chart is based on the provisional data regarding the entering  parliament
-  const { entering } = parliament;
+  // the donut chart is based on the provisional data regarding the entering parliament
+  const { entering } = data;
+
   // target the main container
   const main = d3
     .select('main');
@@ -93,7 +148,79 @@ function showDonutChart() {
     .text('by political affiliation');
 
   // DONUT CHART through svg
-  
+  const margin = {
+    top: 30,
+    right: 30,
+    bottom: 30,
+    left: 30,
+  };
+
+  const width = 500 - (margin.left + margin.right);
+  const height = 280 - (margin.top + margin.bottom);
+
+  const visualization = main
+    .append('svg')
+    .attr('viewBox', `0 0 ${width + (margin.left + margin.right)} ${height + (margin.top + margin.bottom)}`)
+    .append('g')
+    .attr('transform', `translate(${margin.left} ${margin.top})`);
+
+  // pie function
+  const pie = d3
+    .pie()
+    .value(d => d.seats)
+    //establishing the half a donut structure through the startAngle and endAngle functions
+    .startAngle(-Math.PI / 2)
+    .endAngle(Math.PI / 2)
+    // sorting is handled later, massaging the input data
+    .sort(null)
+    .padAngle(0.01);
+
+  // arc function
+  const arc = d3
+    .arc()
+    .innerRadius(width / 10)
+    .outerRadius(width / 2);
+
+  // position the slices at the bottom of the svg
+  const groupSlices = visualization
+    .append('g')
+    .attr('class', 'slices')
+    .attr('transform', `translate(${width/2} ${height})`);
+
+  // ! modify the data to have the last slice (the one dedicated to 'Others') split in two and positioned at the beginning and end of the donunt)
+  // use Object.assign() to avoid modifying the original object
+  const others = Object.assign({}, entering[entering.length - 1]);
+  others.seats /= 2;
+  const enteringData = [
+    others,
+    ...entering
+      .slice(0, entering.length - 1),
+    others,
+  ]
+
+  // add the individual slices binding the pie-ed data
+  const slices = groupSlices
+    .selectAll('g.slice')
+    .data(pie(enteringData))
+    .enter()
+    .append('g')
+    .attr('class', 'slice')
+    .attr('data-party', d => d.data.party);
+
+  // draw the slices through the arc function
+  slices
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', d => d.data.color);
+
+  // include the total number of seats in the bottom center of the visualization
+  const total = entering.reduce((acc, curr) => acc += curr.seats, 0);
+  groupSlices
+    .append('text')
+    .text(total)
+    .attr('text-anchor', 'middle')
+    .style('font-size', '25px')
+    .style('font-weight', 600);
 
   // LEGEND through  a grid
   const legend = main
@@ -105,7 +232,20 @@ function showDonutChart() {
     .data(entering)
     .enter()
     .append('div')
-    .attr('class', 'legend-item');
+    .attr('class', 'legend-item')
+    .style('border-left', (d) => `5px solid ${d.color}`)
+    // when hovering on an item highlight the matching slice in the visualization
+    // ! only the matching slice
+    .on('mouseenter', ({party}) => {
+      d3
+        .selectAll(`g.slice`)
+        .transition()
+        .attr('transform', 'scale(1)');
+      d3
+        .selectAll(`g[data-party="${party}"]`)
+        .transition()
+        .attr('transform', 'scale(1.03)');
+    });
 
   legendItems
     .append('h3')
@@ -114,6 +254,7 @@ function showDonutChart() {
   legendItems
     .append('strong')
     .text(d => d.seats);
+
 }
 
 // function adding the row chart
@@ -122,6 +263,8 @@ function showRowChart() {
 }
 // function showing a specific visualization according to the input value
 function showVisualization(viz = 'donut-chart') {
+  const main = document.querySelector('main');
+  main.innerHTML = '';
   if (viz === 'donut-chart') {
     showDonutChart();
   } else {
