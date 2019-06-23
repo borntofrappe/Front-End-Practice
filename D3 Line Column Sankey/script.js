6a7456a6ea9366d5f8cad4484b1eb6341ae7e5d5
borntofrappe <borntofrappe@gmail.com>
2019-06-23 10:33:12 +0200

    complete the line chart

--- D3 Line Column Sankey/script.js
+++ D3 Line Column Sankey/script.js
@@ -1,7 +1,7 @@
 // data for the line chart, detailing the number of people covered by the UN refugee agency across the years
 const lineData = [2.12, 1.95, 1.85, 1.75, 1.72, 1.77, 1.74, 1.69, 1.67, 1.66, 1.79, 1.77, 1.68, 3.08, 3.57, 3.53, 2.36, 2.49, 2.56, 2.48, 5.34, 13.17, 3.02, 3.12, 3.62, 4.28, 4.52, 5.33, 6.85, 9.23, 9.95, 10.72, 10.72, 11.1, 12.26, 12.96, 13.37, 14.58, 14.94, 17.54, 19.04, 20.08, 21.39, 24.11, 20.36, 20.29, 19.02, 19.15, 19.79, 21.87, 19.9, 20.89, 17.01, 19.52, 21.05, 32.86, 31.68, 34.46, 36.47, 33.92, 35.44, 35.85, 42.87, 54.96, 63.91, 67.64, 71.44, 74.79];
 
-// data for the row chart, describing the resettlements number for the countries with more than 100.000 resettlements
+// data for the row chart, describing the resettlements for the countries with more than 100.000 observations
 const rowData = [
   {
     country: 'Colombia',
@@ -94,28 +94,40 @@ const colors = {
 const href = 'http://popstats.unhcr.org/en/overview';
 
 
-// target the .viz container and add an article for the first visualization
+// PROJECT ORGANIZATION: for each project add an article wrapping HTML elements and the SVG visualization
 
+// tooltip shared by the visualizations
+const tooltip = d3
+  .select('body')
+  .append('div')
+  .attr('id', 'tooltip')
+  .style('position', 'absolute')
+  .style('opacity', 0)
+  .style('visibility', 'hidden')
+  .style('pointer-events', 'none');
+
+// LINE CHART
 const lineChart = d3
   .select('.viz')
   .append('article')
-  .attr('class', 'viz--line');
+  .attr('class', 'viz__line-chart');
 
-// html elements briefly describing the viz
+// html elements briefly describing the visualization and its core message
 const lineChartOverview = lineChart
   .append('div');
 
 lineChartOverview
   .append('h1')
-  .text('A rising trend which doesn\'t slow down');
+  .text('A positive trend which doesn\'t slow down');
 
 lineChartOverview
   .append('p')
-  .text('This chart represents the number of people covered by the High Commissioner for Refugees of the United Nations between 1951 and 2018.');
+  .text('This chart highlights the number of people covered by the UN Refugee Agency between 1951 and 2018.');
+
 
 // svg describing the line chart
 // include the svg following the margin convention
-const margin = {
+const lineChartMargin = {
   top: 20,
   right: 20,
   bottom: 20,
@@ -123,41 +135,35 @@ const margin = {
 };
 
 // describe the width and height to detail a square
-const width = 500 - (margin.left + margin.right);
-const height = 500 - (margin.top + margin.bottom);
+const lineChartWidth = 500 - (lineChartMargin.left + lineChartMargin.right);
+const lineChartHeight = 500 - (lineChartMargin.top + lineChartMargin.bottom);
 
 // svg container
 const lineChartSVG = lineChart
   .append('svg')
-  .attr('viewBox', `0 0 ${width + (margin.left + margin.right)} ${height + (margin.top + margin.bottom)}`);
+  .attr('viewBox', `0 0 ${lineChartWidth + (lineChartMargin.left + lineChartMargin.right)} ${lineChartHeight + (lineChartMargin.top + lineChartMargin.bottom)}`);
 
-// group detailing the elements of the viz
+// group in which to draw the SVG elements
 const lineChartGroup = lineChartSVG
   .append('g')
-  .attr('transform', `translate(${margin.left} ${margin.top})`);
+  .attr('transform', `translate(${lineChartMargin.left} ${lineChartMargin.top})`);
 
 // SCALES
-// linear scale for the y axis, from 0 up to the maximum value of the input data
-const lineChartYScale = d3
-  .scaleLinear()
-  .domain([0, d3.max(lineData)])
-  .range([height, 0]);
-
 // band scale for the x axis, with one band for each data point
 const lineChartXScale = d3
   .scaleBand()
   .domain(d3.range(lineData.length))
-  .range([0, width]);
+  .range([0, lineChartWidth]);
+
+// linear scale for the y axis, from 0 up to the maximum value of the input data
+const lineChartYScale = d3
+  .scaleLinear()
+  .domain([0, d3.max(lineData)])
+  .range([lineChartHeight, 0]);
 
 // AXES
-// based on the scale include the axes
-
-/* style guidelines - x-axis
-- show a limited number of labels,
-- show the number of years starting from 2018 minus the number of data points
-- remove ticks
-- increase padding from the line
-*/
+// for the x axis show the number of years starting from 2018 minus the number of data points
+// ! show a subset of the possible labels
 const lineChartXAxis = d3
   .axisBottom(lineChartXScale)
   .tickFormat((d) => {
@@ -167,29 +173,21 @@ const lineChartXAxis = d3
   .tickSize(0)
   .tickPadding(10);
 
-/* style guidelines - y-axis
-- reduce the number of ticks,
-
-targeting the elements then
-- hide labels
-- hide ticks
-- remove vertical segment
-- include horizontal line spanning the viz from the hidden ticks
-*/
 const lineChartYAxis = d3
   .axisLeft(lineChartYScale)
   .ticks(6);
 
 lineChartGroup
   .append('g')
-  .attr('class', 'axis y-axis')
-  .call(lineChartYAxis);
+  .attr('class', 'axis x-axis')
+  .attr('transform', `translate(0 ${lineChartHeight})`)
+  .call(lineChartXAxis);
 
+// for the y axis remove the segment in favor of text labels and horizontal lines spanning the width
 lineChartGroup
   .append('g')
-  .attr('class', 'axis x-axis')
-  .attr('transform', `translate(0 ${height})`)
-  .call(lineChartXAxis);
+  .attr('class', 'axis y-axis')
+  .call(lineChartYAxis);
 
 d3
   .select('.y-axis')
@@ -207,7 +205,7 @@ d3.select('.y-axis')
 d3.select('.y-axis')
   .selectAll('g.tick')
   .append('path')
-  .attr('d', `M 0 0 h ${width}`)
+  .attr('d', `M 0 0 h ${lineChartWidth}`)
   .attr('fill', 'none')
   .attr('stroke', 'currentColor')
   .attr('stroke-width', '0.5')
@@ -225,7 +223,7 @@ d3.select('.y-axis')
   .text((d, i) => (i % 2 !== 0 ? `${d} Millions` : ''));
 
 // line function
-// using the lineData, create a line function which considers the index for the horizontal dimension and the data point for the vertical dimension
+// leveraging the scales use the index of the data point and its actual value
 const line = d3
   .line()
   .x((d, i) => lineChartXScale(i))
@@ -240,7 +238,7 @@ lineChartGroup
   .attr('stroke-width', 2);
 
 // area function
-// using the same data include an area below the line described earlier
+// using the same data include a path element describing the area below the previous line
 const area = d3
   .area()
   .x((d, i) => lineChartXScale(i))
@@ -254,7 +252,8 @@ lineChartGroup
   .attr('opacity', 0.18);
 
 // for each data point include a group element
-// the idea is to include rectangle elements spanning the entirety of the visualization's height, hide them from view and use them for the hover state to show the tooltip regardless of the vertical coordinate
+// the idea is to include not only circle elements, at the height described by the data point, but also rectangle elements spanning the entirety of the visualization's height
+// the rectangles are made to be transparent and are purely used to show a tooltip regardless of the vertical coordinate
 const lineChartGroups = lineChartGroup
   .selectAll('g.group')
   .data(lineData)
@@ -262,18 +261,68 @@ const lineChartGroups = lineChartGroup
   .append('g')
   .attr('class', 'group')
   .attr('transform', (d, i) => `translate(${lineChartXScale(i)} 0)`)
-  // on hover highlight the nested circle element
-  .on('mouseenter', function (d) {
+  // on hover highlight the nested circle element and show the tooltip
+  .on('mouseenter', function (d, i, {length}) {
     d3
       .select(this)
       .select('circle')
-      .attr('opacity', 1);
+      .attr('opacity', 1)
+      .attr('r', 5.75)
+      .attr('stroke-width', 2.5);
+
+    // show the tooltip detailing the year and value in two paragraph elments
+    tooltip
+      .append('p')
+      .append('strong')
+      .text(2018 - length + i + 1);
+
+    // for the second paragraph include a circle colored with the same hue of the visualizatiojn
+    tooltip
+      .append('p')
+      .style('display', 'flex')
+      .style('align-items', 'center')
+      .html(`
+      <svg width="6" height="6" viewBox="0 0 10 10" style="margin: 0; margin-right: 0.35rem;">
+        <circle cx="5" cy="5" r="5" fill="${colors.line}"></circle>
+      </svg>
+      Persons of concern: ${d} Millions
+      `);
+
+
+    // position the tooltip
+    // horizontally to center the div around the cursor coordinate
+    const width = tooltip.style('width');
+
+    // vertically above the connected circle element
+    // ! since the tooltip is absolutely positioned with respect to the body you need to offset for the distance between the top of the window and the svg visualization
+    const height = tooltip.style('height');
+
+    const {top: verticalOffset} = document.querySelector('.viz__line-chart svg').getBoundingClientRect();
+
+    const tooltipWidth = parseFloat(width.replace(/px/gi, ''));
+    const tooltipHeight = parseFloat(height.replace(/px/gi, ''));
+    tooltip
+      .style('opacity', 1)
+      .style('visibility', 'visible')
+      .style('left', `${d3.event.pageX - tooltipWidth / 2}px`)
+      .style('top', `${lineChartYScale(d) + verticalOffset - tooltipHeight - 25}px`);
+
   })
-  .on('mouseout', function (d, i, l) {
+  // when exiting the group reset the appearance of the circle and remove the tooltip
+  // ! do not hide the circle for the first and last data point
+  .on('mouseout', function (d, i, {length}) {
     d3
       .select(this)
       .select('circle')
-      .attr('opacity', i !== 0 && i !== l.length - 1 ? 0 : 1);
+      .attr('r', 4)
+      .attr('stroke-width', 2)
+      .attr('opacity', i !== 0 && i !== length - 1 ? 0 : 1);
+
+    tooltip
+      .style('opacity', 0)
+      .style('visibility', 'hidden')
+      .selectAll('p')
+      .remove();
   });
 
 // rectangles to show information on hover
@@ -282,7 +331,7 @@ lineChartGroups
   .attr('x', 0)
   .attr('y', 0)
   .attr('width', lineChartXScale.bandwidth())
-  .attr('height', height)
+  .attr('height', lineChartHeight)
   .attr('opacity', 0);
 
 // circle elements to highlight the data point
@@ -295,7 +344,7 @@ lineChartGroups
   .attr('stroke', colors.line)
   .attr('stroke-width', 2)
   .attr('fill', 'hsl(0, 100%, 100%)')
-  .attr('opacity', (d, i, l) => (i === 0 || i === l.length - 1 ? 1 : 0));
+  .attr('opacity', (d, i, {length}) => (i === 0 || i === length - 1 ? 1 : 0));
 
 // link forwarding toward the source URL
 lineChart
