a841dc5c3b781ee00a76d3538ef10074e1afd97f
borntofrappe <borntofrappe@gmail.com>
2019-07-05 00:11:03 +0200

    document existing progress and add svg icon

--- Thank You Animation/script.js
+++ Thank You Animation/script.js
@@ -1,28 +1,30 @@
 /* globals Zdog */
-/* eslint-disable no-new */
-// from the Zdog object extract the necessary objects
+// from the Zdog object extract the necessary modules
 const {
-  Illustration, Ellipse, Shape, Group, Rect, Anchor,
+  Illustration, Ellipse, Rect, Shape, Group, Anchor,
 } = Zdog;
-const canvas = document.querySelector('canvas');
 
-// set up the illustration with the canvas and allow for drag
+// set up the illustration within the existing canvas element
 const illustration = new Illustration({
   element: 'canvas',
   dragRotate: true,
 });
 
+// include an anchor point for the star
+// ! position the star atop the anchor, to have the rotation occur around this point
 const starAnchor = new Anchor({
   addTo: illustration,
   translate: { y: 100 },
   rotate: { z: Math.PI / 10 },
 });
-// draw a star through path elements
+
+// draw a star in a group element positioned atop the anchor point
 const starGroup = new Group({
   addTo: starAnchor,
-  translate: { x: -70, y: -170 },
+  translate: { x: -70, y: -170 }, // -70 to center the 140 wide shape
 });
 
+// draw the path describing the star
 new Shape({
   addTo: starGroup,
   path: [
@@ -39,54 +41,63 @@ new Shape({
     { x: 0, y: 45 },
   ],
   stroke: 40,
-  color: 'hsla(45, 100%, 58%)',
+  color: 'hsl(45, 100%, 58%)',
 });
+// within the path include a rectangle to remove the gap between the center of the star and its stroke
 new Rect({
   addTo: starGroup,
   width: 40,
   height: 50,
   stroke: 40,
   translate: { x: 70, y: 70 },
-  color: 'hsla(45, 100%, 58%)',
+  color: 'hsl(45, 100%, 58%)',
 });
 
-const eyes = new Group({
+// include a group for the eyes, positioned halfway through the height of the star
+const eyesGroup = new Group({
   addTo: starGroup,
   translate: { x: 70, y: 72.5, z: 20 },
 });
+
+// add black circles describing the contour of the eyes, and either end of the star
 const eye = new Ellipse({
-  addTo: eyes,
+  addTo: eyesGroup,
   diameter: 5,
   stroke: 15,
   translate: { x: -32.5 },
-  color: 'hsla(0, 0%, 0%)',
+  color: 'hsl(0, 0%, 0%)',
 });
 eye.copy({
   translate: { x: 32.5 },
 });
 
-const leftIrisAnchor = new Anchor({
-  addTo: eyes,
+// add an anchor point for the white part of the eyes
+// by later translating the white part of the eyes, the rotation allows to have the circle rotate around the anchor point
+const leftEyeAnchor = new Anchor({
+  addTo: eyesGroup,
   translate: { x: -32.5, z: 0.5 },
-  // rotate: { z: Math.PI / 2 },
 });
-const leftIris = new Ellipse({
-  addTo: leftIrisAnchor,
+const leftEye = new Ellipse({
+  addTo: leftEyeAnchor,
   diameter: 1,
   stroke: 5,
-  color: 'hsla(0, 100%, 100%)',
+  color: 'hsl(0, 100%, 100%)',
   translate: { x: -3.5 },
 });
 
-const rightIrisAnchor = leftIrisAnchor.copyGraph({
+// copy the left anchor for the right side
+const rightEyeAnchor = leftEyeAnchor.copyGraph({
   translate: { x: 32.5, z: 0.5 },
-  // rotate: { z: 0 },
 });
 
+// include an anchor point for the mouth
+// by centering the mouth around the anchor and scaling the anchor itself, the change in size occurs from the center of the mouth
 const mouthAnchor = new Anchor({
   addTo: starGroup,
   translate: { x: 70, y: 95, z: 20 },
+  scale: 0.8,
 });
+// draw a mouth with a line and arc commands
 const mouth = new Shape({
   addTo: mouthAnchor,
   path: [
@@ -109,7 +120,7 @@ const mouth = new Shape({
   color: 'hsl(358, 100%, 65%)',
 });
 
-// draw a circle with a fill and no stroke for the shadow
+// below the start draw a circle with a fill and no stroke, for the shadow
 const shadow = new Ellipse({
   addTo: illustration,
   diameter: 100,
@@ -122,11 +133,13 @@ const shadow = new Ellipse({
 
 illustration.updateRenderGraph();
 
-/* to animate the star, change the transform property of the following elements
+/* to animate the star, change the transform property as follows
+
 |variableName|transform|valueRange|
 |---|---|---|
 |starAnchor|rotate.z|[Math.PI/10, -Math.PI/10]|
 |leftIrisAnchor && rightIrisAnchor|rotate.z|[0, Math.PI/2]|
 |mouthAnchor|scale|[1, 1.2]|
 |shadow|translate.x|[50, -50]|
+
 */
