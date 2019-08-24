9f2f2c77ff1b67a22c9be32fc93594f547c0c633
borntofrappe <borntofrappe@gmail.com>
2019-08-25 00:12:02 +0200

    complete the base of the project

--- CPC Connect the Dots/script.js
+++ CPC Connect the Dots/script.js
@@ -1,54 +1,72 @@
-const canvas = document.querySelector('#drawing-canvas');
+// retrieve the canvas and its dimensions
+const canvas = document.querySelector('.worksheet canvas');
 const {width, height} = canvas;
+// context to draw the circle/path elements
 const context = canvas.getContext('2d');
 
+// function called initially and following a click on the trash-can button
 function setupCanvas() {
-  const d = document.querySelector('#drawing-svg path').getAttribute('d');
+  // retrieve the coordinates described by the path element
+  const d = document.querySelector('.base svg path').getAttribute('d');
   const coordinates = d.match(/\d+ \d+/g);
+  // consider the (x, y) integers described by the different sets of coordinates
   const points  = coordinates.map(point => point.split(' ').map((coordinate) => parseInt(coordinate, 10)));
 
+  // for each point draw a circle
   points.forEach((point, index) => {
     const [x, y] = point;
     context.beginPath();
     context.arc(x, y, 4, 0, Math.PI * 2);
     context.fill();
 
+    // below the circle include text describing the order of the point
     context.font = '14px sans-serif';
     context.textAlign = 'center';
     context.fillText(index + 1, x, y + 18);
   });
 }
-
-
+// immediately call the function setting up the canvas
 setupCanvas();
 
-
+// boolean allowing to draw only as the cursor is down on the canvas
 let isCanvasFocused = false;
+// variables describing the position from which the path is drawn
 let startingX, startingY = [0, 0];
+
+
+// function setting up the path
+// ! update the initial coordinates
+function setupPath(x, y) {
+  isCanvasFocused = true;
+  context.lineWidth = 4;
+  [startingX, startingY] = [x, y];
+
+  // ! enable the button by adding the prescribed class
+  button.classList.add('allowed');
+  button.removeAttribute('disabled');
+}
+
+// function drawing the path
 function drawPath(x, y) {
+  // draw a path from the starting coordinates to the new values
   context.beginPath();
   context.moveTo(startingX, startingY)
   context.lineTo(x, y)
   context.stroke();
 
+  // update the starting coordinates to have the stroke continue from where it last was
   [startingX, startingY] = [x, y];
 };
-function setupPath(x, y) {
-  button.classList.add('active');
-  button.removeAttribute('disabled');
 
-  context.lineWidth = 4;
-
-  [startingX, startingY] = [x, y];
-  isCanvasFocused = true;
-
-}
+// add the necessary event listeners
+// as the canvas is focused update the starting position and toggle the control value to true
 canvas.addEventListener('mousedown', ({layerX, layerY}) => setupPath(layerX, layerY));
 canvas.addEventListener('touchstart', (e) => {
   const { layerX, layerY } = e.targetTouches[0];
   setupPath(layerX, layerY);
 });
 
+// as a cursor is moved on the canvas draw the path to the final coordinates
 canvas.addEventListener('mousemove', (e) => {
   if(isCanvasFocused) {
     const { layerX, layerY } = e;
@@ -63,14 +81,19 @@ canvas.addEventListener('touchmove', (e) => {
   }
 });
 
+// as the cursor leaves the canvas element switch the control boolean back to false
 canvas.addEventListener('mouseup', () => { isCanvasFocused = false; });
 canvas.addEventListener('mouseout', () => { isCanvasFocused = false; });
 canvas.addEventListener('touchend', () => { isCanvasFocused = false; });
 
-const button = document.querySelector('button');
+// following a click on the button element clear the content of the canvas
+const button = document.querySelector('.worksheet button');
 button.addEventListener('click', () => {
-  button.classList.remove('active');
-  button.setAttribute('disabled', true);
   context.clearRect(0, 0, width, height);
+  // ! call the setup function to retain the coordinates
   setupCanvas();
+
+  // disable the button
+  button.classList.remove('allowed');
+  button.setAttribute('disabled', true);
 });
