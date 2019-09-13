37ebcbd923e4e5477003c9ccd545d60414250b44
borntofrappe <borntofrappe@gmail.com>
2019-09-14 00:40:39 +0200

    complete the static visualization

--- D3 Band Members/script.js
+++ D3 Band Members/script.js
@@ -127,6 +127,12 @@ const band = [
   },
 ];
 
+
+const timeScale = d3
+  .scaleTime()
+  .domain([new Date('1973'), new Date()])
+  .range([0, 100]);
+
 // add a div container for each member
 const viz = d3.select('.viz');
 
@@ -145,3 +151,85 @@ members
   .attr('height', '60')
   .append('use')
   .attr('href', ({ role }) => `#${role}`);
+
+// add a container in which to describe the name and years of acivity
+const details = members
+  .append('div')
+  .attr('class', 'details');
+
+details
+  .append('h2')
+  .text(({name}) => name)
+  .style('position', 'absolute')
+  .style('top', '40%')
+  .style('left', ({ active }) => {
+    const beginning = new Date(`${active[0].beginning}`);
+    const end = active[active.length - 1].end ? new Date(`${active[active.length - 1].end}`) : new Date();
+    const midPoint = timeScale(beginning) + (timeScale(end) - timeScale(beginning)) / 2;
+    return `${midPoint}%`;
+  })
+  .style('transform', ({ active }) => {
+    const beginning = new Date(`${active[0].beginning}`);
+    const end = active[active.length - 1].end ? new Date(`${active[active.length - 1].end}`) : new Date();
+    const midPoint = timeScale(beginning) + (timeScale(end) - timeScale(beginning)) / 2;
+    if(midPoint < 10) {
+      return 'translate(0%, -50%)';
+    } else if(midPoint > 90) {
+      return 'translate(-100%, -50%)';
+    }
+    return 'translate(-50%, -50%)';
+  });
+
+const years = details
+  .selectAll('div')
+  .data(({active}) => active)
+  .enter()
+  .append('div')
+  .attr('class', 'div')
+  .style('position', 'absolute')
+  .style('top', '0%')
+  .style('width', '100%')
+  .style('height', '100%')
+  .style('left', ({ beginning }) => {
+    return `${timeScale(new Date(`${beginning}`))}%`;
+  });
+
+years
+  .append('span')
+  .style('position', 'absolute')
+  .style('top', '75%')
+  .style('left', '0%')
+  .style('background', 'currentColor')
+  .style('border-radius', '5px')
+  .style('width', '2px')
+  .style('height', '30%')
+  .style('transform', 'translate(-50%, -50%)');
+
+years
+  .append('span')
+  .style('position', 'absolute')
+  .style('top', '75%')
+  .style('left', '0%')
+  .style('background', 'currentColor')
+  .style('border-radius', '5px')
+  .style('width', ({ beginning, end }) => {
+    const left = end ? new Date(`${end}`) : new Date();
+    return `${timeScale(left) - timeScale(new Date(`${beginning}`))}%`;
+  })
+  .style('height', '2px')
+  .style('transform', 'translate(0%, -50%)');
+
+years
+  .append('span')
+  .style('position', 'absolute')
+  .style('top', '75%')
+  .style('left', ({ beginning, end }) => {
+    const left = end ? new Date(`${end}`) : new Date();
+    return `${timeScale(left) - timeScale(new Date(`${beginning}`))}%`;
+  })
+  .style('opacity', ({ end }) => end ? '1' : '0')
+  .style('background', 'currentColor')
+  .style('border-radius', '5px')
+  .style('width', '2px')
+  .style('height', '30%')
+  .style('transform', 'translate(-50%, -50%)');
