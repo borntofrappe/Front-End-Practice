99c3168d3371f60ab48975eedac270d907fcc87e
borntofrappe <borntofrappe@gmail.com>
2019-09-22 00:44:25 +0200

    complete the toggle

---
+++ SVG Toggle/script.js
@@ -0,0 +1,6 @@
+const input = document.querySelector('input[type="checkbox"]');
+function handleInput() {
+  const { checked } = this;
+  document.querySelector('body').style.background = checked ? '#151d29' : '#d6e7f7';
+}
+input.addEventListener('input', handleInput);
