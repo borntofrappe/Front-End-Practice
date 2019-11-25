9acb74708216e7b7cc1865a564c71d120e443fb7
borntofrappe <borntofrappe@gmail.com>
2019-11-26 00:16:07 +0100

    include multiple planets and restructure the project

--- Canvas Clip/script.js
+++ Canvas Clip/script.js
@@ -2,8 +2,16 @@
 const canvasBackground = document.querySelector('canvas:nth-of-type(1)');
 const canvasClip = document.querySelector('canvas:nth-of-type(2)');
 
-const rotation = 0;
+// object describing the position of the clip
+const clip = {
+  x: window.innerWidth / 2,
+  y: window.innerHeight / 2,
+};
+
+
+// function adding a star in the form of an object detailing the position, speed and size of a then circle
 function addStar() {
+  // shape positioned either at the top or the left of the screen
   const fromTheTop = Math.random() > 0.5;
   if (fromTheTop) {
     return {
@@ -22,9 +30,41 @@ function addStar() {
     radius: Math.random() * 3,
   };
 }
+
+// array describing the planets
+const planets = [
+  {
+    x: 1 / 8,
+    y: 1 / 4,
+  },
+  {
+    x: 3 / 4,
+    y: 1 / 6,
+  },
+  {
+    x: 1 / 2,
+    y: 1 / 2,
+  },
+  {
+    x: 1 / 5,
+    y: 3 / 4,
+  },
+  {
+    x: 6 / 7,
+    y: 4 / 5,
+  }
+].map(({x, y}, index, { length }) => ({
+  x,
+  y,
+  color: `${360 / length * index}`,
+}));
+
+
+// array describing the stars
 let shootingStars = Array(50)
   .fill('')
   .map(() => addStar());
+
 
 // function drawing in the canvas making up the background
 // the idea is to draw a starry's sky
@@ -79,10 +119,6 @@ function drawBackground() {
   }
 }
 
-const clip = {
-  x: window.innerWidth / 2,
-  y: window.innerHeight / 2,
-};
 
 // function drawing in the canvas clipped to show only a portion at a time
 // the idea is to then move the clip following the cursor
@@ -91,10 +127,16 @@ function drawClip() {
   const { width, height } = canvasClip;
   const context = canvasClip.getContext('2d');
 
+  // clear the previous drawing
   context.clearRect(0, 0, width, height);
+
+  // wrap the clipped section in a save-restore pair to avoid repeating
   context.save();
+
+  // describe the radius using the updated width of the canvas
   const radius = Math.min(150, width / 2);
 
+  // "draw" the clip
   context.lineWidth = 20;
   const { x, y } = clip;
   context.beginPath();
@@ -102,19 +144,24 @@ function drawClip() {
   context.closePath();
   context.clip();
 
-  context.save();
-  const radialGradient = context.createRadialGradient(0, 0, 80, 80, -80, 80);
+  // draw the planets
+  // include a radial gradient next to each planet
+  planets.forEach((planet) => {
+    context.save();
+    context.translate(planet.x * width, planet.y * height)
+    const radialGradient = context.createRadialGradient(0, 0, 80, 80, -80, 80);
+    radialGradient.addColorStop(0, `hsl(${planet.color}, 80%, 10%)`);
+    radialGradient.addColorStop(1, `hsl(${planet.color}, 80%, 70%)`);
+    context.fillStyle = radialGradient;
+    context.beginPath();
+    context.arc(0, 0, 50, 0, Math.PI * 2);
+    context.closePath();
+    context.fill();
+    context.restore();
 
-  radialGradient.addColorStop(0, 'hsl(60, 80%, 10%)');
-  radialGradient.addColorStop(1, 'hsl(60, 80%, 70%)');
-  context.translate(width / 2, height / 2);
-  context.fillStyle = radialGradient;
-  context.beginPath();
-  context.arc(0, 0, 50, 0, Math.PI * 2);
-  context.closePath();
-  context.fill();
-  context.restore();
+  });
 
+  // draw the stars
   shootingStars.forEach((shootingStar, index) => {
     context.fillStyle = 'hsl(60, 90%, 70%)';
     context.beginPath();
@@ -128,6 +175,7 @@ function drawClip() {
     context.closePath();
     context.fill();
 
+    // move the shotting stars and whenever a copy exceeds the boundaries of the window, remove it in favor of a new one
     shootingStar.x += shootingStar.vx;
     shootingStar.y += shootingStar.vy;
 
@@ -140,7 +188,7 @@ function drawClip() {
     }
   });
 
-  // circle fabricating the lens of the telescope
+  // draw a circle to fake a lens above the clip
   context.strokeStyle = 'hsl(240, 100%, 5%)';
   context.fillStyle = 'hsla(0, 0%, 100%, 0.05)';
   context.beginPath();
@@ -168,11 +216,17 @@ function resize() {
     canvas.height = window.innerHeight;
   });
 
-  // re-draw the elements in the newly sized canvases
-  drawBackground();
 }
-window.addEventListener('resize', () => resize());
+
+window.addEventListener('resize', () => {
+  resize();
+  // re-draw the background
+  // the clipped canvas is drawn after requestAnimationFrame
+  drawBackground();
+});
 window.addEventListener('mousemove', updateClip);
 
-drawClip();
+// resize the canvases and draw the connecting elements
 resize();
+drawClip();
+drawBackground();
