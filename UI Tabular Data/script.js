3665b3b8be7bed9ebcbc64e52a462b65ef134048
borntofrappe <borntofrappe@gmail.com>
2019-12-03 23:48:30 +0100

    update the ui of the table

--- UI Tabular Data/script.js
+++ UI Tabular Data/script.js
@@ -1,4 +1,4 @@
-// tabular data
+// object describing the alphabet through the dots and dashes matching each letter
 const alphabet = {
     a: '.-',
     b: '-...',
@@ -28,35 +28,45 @@ const alphabet = {
     z: '--..',
 };
 
+// multi-dimensional array describing the letters and codes in pairs
 const entries = Object.entries(alphabet);
 
-const svgMarkup = (code, addOffset = false) => {
-    const width = 5;
+// utility function generating the svg syntax for the morse code
+// the idea is to use path elements to have the possibility of animating the drawing through stroke-dash properties
+const svgMarkup = (code) => {
+    // for each code add an SVG element with a fixed height, but a variable width
     const height= 5;
+    const x = 5;
+    const width = x * code.length;
+
+    // ! given stroke-linecap="round" and the stroke-width property, creating a path element through `h 0` allows to draw a dot
     return `
-    <svg viewBox="0 0 ${width * code.length} ${height}" width="${width * code.length * 5}" height="${height * 5}">
-        ${code.split('').map((letter, index) => `
+    <svg viewBox="0 -${height / 2} ${width} ${height}" width="${width}" height="${height}">
         <g stroke="currentColor" stroke-linecap="round" stroke-width="${height / 4}">
-            <g transform="translate(${width * index} 0)">
+        ${code.split('').map((letter, index) => `
+            <g transform="translate(${x * index} 0)">
                 ${letter === '.'
                 ?
-                `<path d="M ${width / 2} ${height / 2} h 0" ${addOffset && `stroke-dasharray="${height / 4}"  stroke-dashoffset="${height / 4}"`} />`
+                `<path d="M ${x / 2} 0 h 0"/>`
                 :
-                `<path d="M ${width / 4} ${height / 2} h ${width / 2}"  ${addOffset && `stroke-dasharray="${width / 2}"  stroke-dashoffset="${width / 2}"`} />`
+                `<path d="M ${x / 4} 0 h ${x / 2}" />`
                 }
             </g>
+            `).join('')}
         </g>
-        `).join('')}
     </svg>
 `;
 };
+
+
+// table describing the alphabet and codes, using the svgMarkup function
 const markup = `
     <table>
         <thead>
             <tr>
                 <th>Letter</th>
                 <th>Code</th>
-                <th>Animation</th>
+                <th>Send Letter</th>
             </tr>
         </thead>
         <tbody>
@@ -65,12 +75,15 @@ const markup = `
                     <td>${letter}</td>
                     <td>${svgMarkup(code)}</td>
                     <td>
-                        <button>Play</button>
-                        ${svgMarkup(code, true)}
+                        <button aria-label="Send letter in morse code">
+                            <svg viewBox="0 0 100 100" width="20" height="20">
+                                <path d="M 25 10 l 60 40 -60 40 z"/>
+                            </svg>
+                        </button>
                     </td>
                 </tr>
             `).join('')}
         </tbody>
     </table>
 `;
-document.querySelector('#root').innerHTML = markup;
+document.querySelector('body').innerHTML = markup;
