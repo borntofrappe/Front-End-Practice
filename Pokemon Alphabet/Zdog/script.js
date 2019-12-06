38f49529d8b2489bcb23c1a677392926f08e04b5
borntofrappe <borntofrappe@gmail.com>
2019-12-06 01:06:51 +0100

    complete the zdog project

--- Pokemon Alphabet/Zdog/script.js
+++ Pokemon Alphabet/Zdog/script.js
@@ -1,4 +1,4 @@
-const { Illustration, Ellipse, Shape, Group, Anchor } = Zdog;
+const { Illustration, Ellipse, Shape, Group, TAU } = Zdog;
 
 const element = document.querySelector('canvas');
 
@@ -7,42 +7,80 @@ const illustration = new Illustration({
     dragRotate: true,
 });
 
-
-const i = new Group({
+const letter = new Group({
     addTo: illustration,
 })
 
-new Shape({
-    addTo: i,
-    stroke: 15,
-    path: [{ x: 0, y: -60 }, { x: 0, y: 60 }]
-})
-
-
 new Ellipse({
-    addTo: i,
+    addTo: letter,
     diameter: 25,
-    stroke: 25,
+    stroke: 30,
     color: '#fff',
 });
-
-
 new Ellipse({
-    addTo: i,
-    diameter: 60,
+    addTo: letter,
+    diameter: 70,
     stroke: 15,
-    color: '#3E3A39',
+    color: 'hsl(0, 0%, 15%)',
 });
 
 new Ellipse({
-    addTo: i,
+    addTo: letter,
     stroke: 15,
-    color: '#3E3A39',
+    color: 'hsl(0, 0%, 15%)',
 });
 
 
+const leg = new Shape({
+    addTo: letter,
+    stroke: 15,
+    color: 'hsl(0, 0%, 15%)',
+    path: [
+        { x: -3, y: 35 },
+        { x: -3, y: 80 },
+        { arc: [
+            { x:  -3, y: 95 },
+            { x:  -15, y:  95 },
+          ]},
+    ],
+    closed: false,
+});
+
+leg.copy({
+    scale: { x: -1}
+})
+
+new Shape({
+    addTo: letter,
+    stroke: 15,
+    color: 'hsl(0, 0%, 15%)',
+    path: [
+        { x: 0, y: 60 },
+        { x: 30, y: 60 },
+    ],
+    closed: false,
+});
+
+new Shape({
+    addTo: letter,
+    stroke: 15,
+    color: 'hsl(0, 0%, 15%)',
+    path: [
+        { x: 35, y: 0 },
+        { x: 90, y: 0 },
+        { x: 65, y: 25 },
+    ],
+});
+
+
+// animate the shape to bobble up and down
+let counter = 1;
 function animate() {
+    counter = counter >= 6.28 ? 0 : counter + 0.05;
+    letter.translate.y = Math.cos(counter) * 5;
+    letter.translate.x = Math.sin(counter) * 5;
     illustration.updateRenderGraph();
     requestAnimationFrame(animate);
 }
 animate();
+
