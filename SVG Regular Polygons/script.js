011e3164c88780bb1a78e8c49e0a180bdfb6b727
borntofrappe <borntofrappe@gmail.com>
2019-12-08 00:14:31 +0100

    document codebase and style application

--- SVG Regular Polygons/script.js
+++ SVG Regular Polygons/script.js
@@ -1,9 +1,16 @@
+/* function creating the sides of the regular polygons
+the idea is to recursively add horizontal lines which are rotated according to the input angle
+rotate -> horizontal line -> translate
+    repeat until you reach the starting point
+*/
 const segments = (sidesLeft, side, angle) => `<g transform="rotate(-${angle})">
     <path d="M 0 0 h ${side}"/>
     <g transform="translate(${side} 0)">
         ${sidesLeft === 1 ? '' : segments(sidesLeft - 1, side, angle)}
     </g>
 </g>`;
+
+// function setting up the svg for the regular polygon
 const regularPolygon = sides => {
   const radius = 50;
   const diameter = radius * 2;
@@ -28,23 +35,43 @@ const regularPolygon = sides => {
   `;
 };
 
+
+// markup
+/* in the form element add the following structure
+div
+    svg
+h1
+input[type="range"]
+
+*/
 const form = document.querySelector('form');
 
-const svgWrapper = document.createElement('div');
-svgWrapper.innerHTML = regularPolygon(3);
+// in the div container add a polygon with an arbitrary number of sides
+const initialSides = Math.floor(Math.random() * 6) + 3;
+const divContainer = document.createElement('div');
+divContainer.innerHTML = regularPolygon(initialSides);
 
+// in the heading describe the number
+const headingSides = document.createElement('h1');
+headingSides.textContent = `Sides: ${initialSides}`;
+
+// for the input set a sensible minimum and maximum value
 const inputRange = document.createElement('input');
 inputRange.setAttribute('type', 'range');
 inputRange.setAttribute('id', 'sides');
 inputRange.setAttribute('min', '3');
 inputRange.setAttribute('max', '20');
-inputRange.setAttribute('value', '3');
+inputRange.setAttribute('value', initialSides);
 
-form.appendChild(svgWrapper);
+form.appendChild(divContainer);
+form.appendChild(headingSides);
 form.appendChild(inputRange);
 
+// following the input event on the form take the value from the input#sides and include the new svg graphic
 function handleInput(e) {
-  svgWrapper.innerHTML = regularPolygon(this.sides.value);
+    const { value } = this.sides;
+  divContainer.innerHTML = regularPolygon(value);
+  headingSides.textContent = `Sides: ${value}`;
 }
 
 form.addEventListener('input', handleInput);
