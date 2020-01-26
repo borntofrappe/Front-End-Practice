a4931585d9dac26abce6e67ce2c4fe557f4dbf97
borntofrappe <borntofrappe@gmail.com>
2020-01-27 00:17:32 +0100

    update grid and document code

--- UI Photo Grid/script.js
+++ UI Photo Grid/script.js
@@ -1,23 +1,31 @@
+// target the anchor links and div container
 const links = document.querySelectorAll('a');
-const modal = document.querySelector('div.modal');
-function showModal() {
-  console.log(this);
+const overlay = document.querySelector('div');
+
+// following a click event on the links show the overlay
+function showOverlay() {
+  // use the src & alt attribute of the selected image in the overlay
   const src = this.querySelector('img').getAttribute('src');
   const alt = this.querySelector('img').getAttribute('alt');
-  modal.querySelector('img').setAttribute('src', src);
-  modal.querySelector('img').setAttribute('alt', alt);
-  console.log(src);
-  modal.classList.add('show');
+  overlay.querySelector('img').setAttribute('src', src);
+  overlay.querySelector('img').setAttribute('alt', alt);
+
+  overlay.classList.add('overlay');
+
+  // to remove the anchor links from reach set the tabindex attribute to a negative value
   links.forEach(link => link.setAttribute('tabindex', -1));
 }
 
-links.forEach(link => link.addEventListener('click', showModal));
+links.forEach(link => link.addEventListener('click', showOverlay));
 
-function closeModal(e) {
+// following a click event on the overlay, consider if the click was registered on the button
+// if so hide the overlay back
+function hideOverlay(e) {
   if (e.target.tagName === 'BUTTON') {
-    modal.classList.remove('show');
-    links.forEach(link => link.setAttribute('tabindex', 1));
+    overlay.classList.remove('overlay');
+    // restore the default value of the anchor links
+    links.forEach(link => link.setAttribute('tabindex', 0));
   }
 }
 
-modal.addEventListener('click', closeModal);
+overlay.addEventListener('click', hideOverlay);
