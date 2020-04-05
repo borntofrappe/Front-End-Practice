ec72692423289ab1ab090cf746616e9aea997e45
borntofrappe <borntofrappe@gmail.com>
2020-04-06 00:11:52 +0200

    complete script

--- JS Template/README.md
+++ JS Template/README.md
@@ -5,3 +5,38 @@
 ## Goal
 
 Use template literals to replicate [these exercises](https://developers.google.com/tech-writing/one/just-enough-grammar#exercise_1) on technical writing.
+
+## Notes
+
+This project was inspired by the [#codepenchallenge](https://codepen.io/challenges/2020/march/3) for the third week of March, as well as [this insightful course](https://developers.google.com/tech-writing) on technical writing.
+
+The idea is to show an exercise
+
+```html
+Samantha is coding Operation Bullwinkle in C++.
+```
+
+And then highlight the desired keywords through a `<strong>` element.
+
+```html
+Samantha <strong>is coding</strong> Operation Bullwinkle in C++.
+```
+
+In this trivial example the task of the exercise is to highlight the verbs, hence the wrapping element around the verb **is coding**.
+
+## Markup
+
+```pug
+#root
+  h1 Grammar Exercises
+  p inspired by
+    a(href="...") this insightful course
+  article
+    h2 Exercise
+    p {assignment}
+    p {passage}
+    details
+      summary Click to see the answer
+      p {answer}
+        strong {matches}
+```
--- JS Template/script.js
+++ JS Template/script.js
@@ -1,9 +1,74 @@
-const data = [{
-  text: 'The cafeteria featured peashew butter and pluot jam on pumperye toast. Employees found it awesome and wished they could eat this every day.',
-  match: ['it', 'they', 'this'],
-},
-{
-  text: 'Samantha is coding Operation Bullwinkle in C++. This project currently consumes over 80,000 lines of code. She previously used Python, but recently gravitated to C++. Samantha leads a team of four software engineers, which will grow to six software engineers next quarter.',
-  match: ['is coding', 'consumes', 'used', 'gravitated', 'leads', 'will grow'],
-},
+const href = 'https://developers.google.com/tech-writing';
+
+const exercises = [
+  {
+    assignment: 'Identify the three pronouns in the following passage:',
+    passage:
+      'The cafeteria featured peashew butter and pluot jam on pumperye toast. Employees found it awesome and wished they could eat this every day.',
+    matches: ['it', 'they', 'this'],
+  },
+  {
+    assignment: 'Identify the verbs in the following passage:',
+    passage:
+      'Samantha is coding Operation Bullwinkle in C++. This project currently consumes over 80,000 lines of code. She previously used Python, but recently gravitated to C++. Samantha leads a team of four software engineers, which will grow to six software engineers next quarter.',
+    matches: [
+      'is coding',
+      'consumes',
+      'used',
+      'gravitated',
+      'leads',
+      'will grow',
+    ],
+  },
 ];
+
+/* markup
+article
+  h2
+  p {assignment}
+  p {passage}
+  details
+    summary Click
+    p {answer}
+      strong {matches}
+
+*/
+
+const markupExercise = ({ assignment, passage, matches }) => {
+  let answer = passage;
+  matches.forEach(match => {
+    const regex = new RegExp(match, 'gi');
+    answer = answer.replace(regex, match => `<strong>${match}</strong>`);
+  });
+
+  return `
+  <article>
+    <h2>Exercise</h2>
+    <p>${assignment}</p>
+    <p>${passage}</p>
+    <details>
+      <summary>
+        Click to see the answer
+      </summary>
+      <p>
+        ${answer}
+      </p>
+    </details>
+  </article>
+  `;
+};
+
+document.body.innerHTML = `
+  <div id="root">
+    <h1>Grammar Exercises</h1>
+    <p>Inspired by
+      <a href="${href}">
+        this insightful course
+      </a>
+      on technical writing
+    </p>
+
+    ${exercises.map(exercise => markupExercise(exercise)).join('')}
+  </div>
+
+`;
