7258e5e722becf361807717197eb1c6064b8018f
borntofrappe <borntofrappe@gmail.com>
2020-04-05 23:17:55 +0200

    introduce js project

---
+++ JS Template/README.md
@@ -0,0 +1,7 @@
+# JS Template
+
+<!-- ## [Live Demo]() -->
+
+## Goal
+
+Use template literals to replicate [these exercises](https://developers.google.com/tech-writing/one/just-enough-grammar#exercise_1) on technical writing.
---
+++ JS Template/script.js
@@ -0,0 +1,9 @@
+const data = [{
+  text: 'The cafeteria featured peashew butter and pluot jam on pumperye toast. Employees found it awesome and wished they could eat this every day.',
+  match: ['it', 'they', 'this'],
+},
+{
+  text: 'Samantha is coding Operation Bullwinkle in C++. This project currently consumes over 80,000 lines of code. She previously used Python, but recently gravitated to C++. Samantha leads a team of four software engineers, which will grow to six software engineers next quarter.',
+  match: ['is coding', 'consumes', 'used', 'gravitated', 'leads', 'will grow'],
+},
+];
