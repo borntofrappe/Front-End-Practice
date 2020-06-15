4af15b5ec5c1efd62cf57a41580ba4d3e44957c4
borntofrappe <borntofrappe@gmail.com>
2020-06-15 23:54:58 +0200

    add the svg icons and css animation

--- UI Time App Navigation/script.js
+++ UI Time App Navigation/script.js
@@ -2,6 +2,6 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         buttons.forEach(button => button.classList.remove('active'));
-        this.classList.add('active');
+        button.classList.add('active');
     })
 })
